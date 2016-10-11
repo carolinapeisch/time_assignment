3092b7e1b2283f49d388ecba755e59c19a6e32bc
Carolina Peisch <carolinapeisch@gmail.com>
2016-10-10 20:37:11 -0400

    more changes

--- time/route.py
+++ time/route.py
@@ -2,24 +2,15 @@
 #https://code.tutsplus.com/tutorials/an-introduction-to-pythons-flask-framework--net-28822
 
 from flask import Flask, render_template
-import datetime
-
+import time
+import sys
  
 app = Flask(__name__)      
  
 @app.route('/')
-def home():
-  return render_template('time_file.py')
+def get_time():
+  return str(time.time()*1000)
  
 if __name__ == '__main__':
   app.run(port=sys.argv[1])
 
-
-
-epoch = datetime.datetime.utcfromtimestamp(0)
-
-def unix_time_millis(dt):
-    return (dt - epoch).total_seconds() * 1000.0
-
-
-print unix_time_millis(unix_time)
